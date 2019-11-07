bb63c2c26ce0cd74d1079f65889e42d98517eb4c
n-bhasin <nkbhasin28@gmail.com>
2019-11-07 08:26:15 -0500

    settings

--- tele_caller/settings.py
+++ tele_caller/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 django_heroku.settings(locals())
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/finsarthi.herokuapp.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'documents')
 MEDIA_URL = '/documents/'
