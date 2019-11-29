c6e83b789f17edec10cf448525381bd501c1de72
n-bhasin <nkbhasin28@gmail.com>
2019-11-29 06:06:01 -0500

    file upload

--- tele_caller/decorator.py
+++ tele_caller/decorator.py
@@ -6,10 +6,6 @@ from website import views
 def role_required(allowed_roles):
 	def decorator(view_func):
 		def wrap(request, *args, **kwargs):
-			print('allowed_roles: ',allowed_roles)
-			print('user: ',request.user)
-			print('superuser: ', request.user.is_superuser)
-			print('status: ',request.user.is_superuser == allowed_roles)
 			if request.user.is_superuser == allowed_roles:
 
 				return view_func(request, *args, **kwargs)
